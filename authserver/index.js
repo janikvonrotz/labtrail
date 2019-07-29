ea585ac01e0db5cf752d2910c2ff229119ee9df3
Janik von Rotz <login@janikvonrotz.ch>
2019-07-29 16:22:21 +0200

    update auth server

---
+++ authserver/index.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const saml = require('samlify');
+const express = require('express');
+const router = express.Router();
+const ServiceProvider = saml.ServiceProvider;
+const IdentityProvider = saml.IdentityProvider;
+const app = express();
+
+// Configure your endpoint for IdP-initiated / SP-initiated SSO
+const sp = ServiceProvider({
+    metadata: fs.readFileSync('./sp.xml', 'utf8')
+});
+const idp = IdentityProvider({
+    metadata: fs.readFileSync('./idp.xml', 'utf8')
+});
+
+// Release the metadata publicly
+router.get('/metadata', (req, res) => res.header('Content-Type', 'text/xml').send(sp.getMetadata()));
+
+// Access URL for implementing SP-init SSO
+router.get('/spinitsso-redirect', (req, res) => {
+    const { id, context } = sp.createLoginRequest(idp, 'redirect');
+    return res.redirect(context);
+});
+
+// If your application only supports IdP-initiated SSO, just make this route is enough
+// This is the assertion service url where SAML Response is sent to
+router.post('/acs', async (req, res) => {
+
+    try {
+        const { id, context: SAMLResponse } = await sp.createLoginRequest(idp, 'post');
+        console.log(SAMLResponse)
+        const { extract } = await sp.parseLoginResponse(idp, 'post', { body: { SAMLResponse }});
+        console.log(extract.attributes);
+        /**
+        *
+        * Implement your logic here. 
+        * extract.attributes, should contains : firstName, lastName, email, uid, groups 
+        *           
+        **/
+      } catch (e) {
+        console.error('[FATAL] when parsing login response sent from OneLogin', e);
+        return res.redirect('/');
+      }
+});
+
+app.use('/sso', router);
+
+module.exports = app;
