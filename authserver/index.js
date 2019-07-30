14abaeb29ab71f5933d1a33c31637d3f11431a37
Janik von Rotz <contact@janikvonrotz.ch>
2019-07-30 15:01:16 +0200

    remove semi and update path for authserver

--- authserver/index.js
+++ authserver/index.js
@@ -1,38 +1,39 @@
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const saml = require('samlify');
-const express = require('express');
-const router = express.Router();
-const ServiceProvider = saml.ServiceProvider;
-const IdentityProvider = saml.IdentityProvider;
-const app = express();
+const fs = require('fs')
+const { join } = require('path')
+const bodyParser = require('body-parser')
+const saml = require('samlify')
+const express = require('express')
+const router = express.Router()
+const ServiceProvider = saml.ServiceProvider
+const IdentityProvider = saml.IdentityProvider
+const app = express()
 
 // Configure your endpoint for IdP-initiated / SP-initiated SSO
 const sp = ServiceProvider({
-    metadata: fs.readFileSync('./sp.xml', 'utf8')
-});
+    metadata: fs.readFileSync(join(__dirname, 'sp.xml'), 'utf8')
+})
 const idp = IdentityProvider({
-    metadata: fs.readFileSync('./idp.xml', 'utf8')
-});
+    metadata: fs.readFileSync(join(__dirname, 'idp.xml'), 'utf8')
+})
 
 // Release the metadata publicly
-router.get('/metadata', (req, res) => res.header('Content-Type', 'text/xml').send(sp.getMetadata()));
+router.get('/metadata', (req, res) => res.header('Content-Type', 'text/xml').send(sp.getMetadata()))
 
 // Access URL for implementing SP-init SSO
 router.get('/spinitsso-redirect', (req, res) => {
-    const { id, context } = sp.createLoginRequest(idp, 'redirect');
-    return res.redirect(context);
-});
+    const { id, context } = sp.createLoginRequest(idp, 'redirect')
+    return res.redirect(context)
+})
 
 // If your application only supports IdP-initiated SSO, just make this route is enough
 // This is the assertion service url where SAML Response is sent to
 router.post('/acs', async (req, res) => {
 
     try {
-        const { id, context: SAMLResponse } = await sp.createLoginRequest(idp, 'post');
+        const { id, context: SAMLResponse } = await sp.createLoginRequest(idp, 'post')
         console.log(SAMLResponse)
-        const { extract } = await sp.parseLoginResponse(idp, 'post', { body: { SAMLResponse }});
-        console.log(extract.attributes);
+        const { extract } = await sp.parseLoginResponse(idp, 'post', { body: { SAMLResponse }})
+        console.log(extract.attributes)
         /**
         *
         * Implement your logic here. 
@@ -40,11 +41,11 @@ router.post('/acs', async (req, res) => {
         *           
         **/
       } catch (e) {
-        console.error('[FATAL] when parsing login response sent from OneLogin', e);
-        return res.redirect('/');
+        console.error('[FATAL] when parsing login response sent from OneLogin', e)
+        return res.redirect('/')
       }
-});
+})
 
-app.use('/sso', router);
+app.use('/sso', router)
 
-module.exports = app;
+module.exports = app
