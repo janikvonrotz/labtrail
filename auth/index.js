677dd9430ab5dd3a94ad699ffe6db5f146672885
Janik von Rotz <contact@janikvonrotz.ch>
2019-08-30 11:15:53 +0200

    lint auth with standard

--- auth/index.js
+++ auth/index.js
@@ -13,10 +13,10 @@ const IdentityProvider = saml.IdentityProvider
 
 // Configure your endpoint for IdP-initiated / SP-initiated SSO
 const sp = ServiceProvider({
-    metadata: fs.readFileSync(join(__dirname, 'sp.xml'), 'utf8')
+  metadata: fs.readFileSync(join(__dirname, 'sp.xml'), 'utf8')
 })
 const idp = IdentityProvider({
-    metadata: fs.readFileSync(join(__dirname, 'idp.xml'), 'utf8')
+  metadata: fs.readFileSync(join(__dirname, 'idp.xml'), 'utf8')
 })
 
 // Create SSO router
@@ -27,45 +27,42 @@ router.get('/metadata', (req, res) => res.header('Content-Type', 'text/xml').sen
 
 // Access URL for implementing SP-init SSO
 router.get('/spinitsso-redirect', (req, res) => {
-    const { id, context } = sp.createLoginRequest(idp, 'redirect')
-    return res.redirect(context)
+  const { id, context } = sp.createLoginRequest(idp, 'redirect')
+  return res.redirect(context)
 })
 
 // If your application only supports IdP-initiated SSO, just make this route is enough
 // This is the assertion service url where SAML Response is sent to
 router.post('/acs', async (req, res) => {
+  try {
+    const { id, context: SAMLResponse } = await sp.createLoginRequest(idp, 'post')
+    console.log(SAMLResponse)
+    const { extract } = await sp.parseLoginResponse(idp, 'post', { body: { SAMLResponse } })
+    console.log(extract.attributes)
 
-    try {
-        const { id, context: SAMLResponse } = await sp.createLoginRequest(idp, 'post')
-        console.log(SAMLResponse)
-        const { extract } = await sp.parseLoginResponse(idp, 'post', { body: { SAMLResponse }})
-        console.log(extract.attributes)
-
-        // Implement your logic here. 
-        // extract.attributes, should contains : firstName, lastName, email, uid, groups 
-
-      } catch (e) {
-        console.error('[FATAL] when parsing login response sent from OneLogin', e)
-        return res.redirect('/')
-      }
+    // Implement your logic here.
+    // extract.attributes, should contains : firstName, lastName, email, uid, groups
+  } catch (e) {
+    console.error('[FATAL] when parsing login response sent from OneLogin', e)
+    return res.redirect('/')
+  }
 })
 
 // Generate JWT mock token
 router.get('/get-mock-token', (req, res) => {
+  // Get email from query param
+  const email = req.query.email
 
-    // Get email from query param
-    const email = req.query.email
+  // Send error if no email
+  if (!email) {
+    res.status(500).send('No email param set.')
+  }
 
-    // Send error if no email
-    if (!email) {
-        res.status(500).send('No email param set.')
-    }
+  // Generate token
+  const token = jwt.sign({ email: email }, process.env.JWT_SECRET)
 
-    // Generate token
-    const token = jwt.sign({ email: email }, process.env.JWT_SECRET )
-    
-    // Return token
-    res.send(token)
+  // Return token
+  res.send(token)
 })
 
 // Add SSO router to express app
